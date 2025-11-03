f4f9d4e7c2bb197d6bdf6ee5c42f7c4d6868a13c
DANIELE SILVA SANTOS <25203622@CFP127ALUNO.LOCAL>
2025-11-03 11:26:22 -0300

    componentes adicionados

--- src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
+++ src/main/java/br/senai/sp/jandira/tabuada/HelloApplication.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -27,10 +30,27 @@ public class HelloApplication extends Application {
         //Colocar o header no root
         root.getChildren().addAll(header);
 
+        //Criar o multiplicando
+        HBox multiplicandoBox = new HBox();
+        multiplicandoBox.setStyle("-fx-label-padding: 10;");
+        Label labelMultiplicando = new Label("Multiplicando");
+        TextField textFieldMultiplicando = new TextField();
+
+        multiplicandoBox.getChildren().addAll(labelMultiplicando, textFieldMultiplicando);
+
+        //Colocamos o multiplicandoBox no root
+        root.getChildren().addAll(multiplicandoBox);
+
+
         //Adicionar um label ao header
         Label labelTitulo = new Label("Tabuada");
         labelTitulo.setStyle("-fx-text-fill: white; -fx-font-size: 25; -fx-font-weight: Bold");
+        Label labelSubtitulo = new Label("Construa tabuadas sem limites!");
+        labelSubtitulo.setStyle("-fx-text-fill: white");
+
         header.getChildren().add(labelTitulo);
+        header.getChildren().add(labelSubtitulo);
+
 
         stage.setScene(scene);
         stage.setTitle("Tabuada");
